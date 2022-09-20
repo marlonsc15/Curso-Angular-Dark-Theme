d213d2728b56fb8c527e7f7305860e9d6ba4ae67
marlonsc15 <marlonsantoscorreia@gmail.com>
2022-09-20 14:51:30 -0300

    pagina home e sobre finalizados

--- Dark-Theme/src/app/app-routing.module.ts
+++ Dark-Theme/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { SobreComponent } from './core/components/sobre/sobre.component';
+import { HomeComponent } from './core/components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'sobre',
+    component: SobreComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
